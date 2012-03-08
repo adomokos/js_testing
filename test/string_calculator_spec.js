ff0d31d53b9028130b789d810df08e2f00e443ac
Attila Domokos <adomokos@gmail.com>
2012-03-08 13:29:45 -0500

    Making Mocha available for browser testing

--- test/string_calculator_spec.js
+++ test/string_calculator_spec.js
@@ -1,40 +1,30 @@
-var jsdom = require('jsdom');
-global.window = jsdom.jsdom().createWindow();
-var should = require('should');
-
-var StringCalculatorApp = require('../public/js/string_calculator');
-
 describe("StringCalculator", function() {
-
-  beforeEach(function() {
-    this.StringCalculator = window.StringCalculator;
-  });
 
   describe("when an empty string is passed in", function() {
     it("returns 0", function() {
       var result = StringCalculator.add("");
-      result.should.equal(0);
+      assert(result === 0);
     });
   });
 
   describe("when a number is passed in", function() {
     it("returns the number", function() {
       var result = StringCalculator.add("2");
-      result.should.equal(2);
+      assert(result === 2);
     });
   });
 
   describe("when string is passed in", function() {
     it("returns NaN", function() {
       var result = StringCalculator.add("a");
-      isNaN(result).should.be.true;
+      assert(isNaN(result));
     });
   });
 
   describe("when '1,2' is passed in", function() {
     it("returns 3", function() {
       var result = StringCalculator.add("1,2");
-      result.should.equal(3);
+      assert(result === 3);
     });
-  })
+  });
 });
